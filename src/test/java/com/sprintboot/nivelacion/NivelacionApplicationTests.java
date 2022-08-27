876dcb4a659d786d924693c27aa42171e13b2cc8
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2022-08-26 23:27:23 -0500

    Sin cambios aun

---
+++ src/test/java/com/sprintboot/nivelacion/NivelacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintboot.nivelacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NivelacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
